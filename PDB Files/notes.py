86dfb7fba1c8def6b8bfce6564245df988cb6d77
Joshua Pillai <jppillai@ucsd.edu>
2024-12-11 09:19:01 -0800

    Create notes.py

---
+++ PDB Files/notes.py
@@ -0,0 +1,5 @@
+# The source files for the WT TREM2 structure prediction are provided from the AlphaFold Server. 
+
+# We then took the .cif files for the 5 predictions and adjusted them to .pdb files with ChimeraX.
+
+# All subsequent analyses were conducted with PyMOL or other softwares.
