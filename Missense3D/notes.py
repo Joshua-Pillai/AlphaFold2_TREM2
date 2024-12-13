df4545172d00752343030701ecd86dde8ac86123
Joshua Pillai <jppillai@ucsd.edu>
2024-12-12 23:23:02 -0800

    Update notes.py

--- Missense3D/notes.py
+++ Missense3D/notes.py
@@ -1,5 +1,6 @@
 # File Paths 
 V27M: PDB-fold_wt_trem2_model0-A-27MET.sc.pdb
 A28V: PDB-fold_wt_trem2_model0-A-28VAL.sc.pdb
-S31F:
+S31F: PDB-fold_wt_trem2_model0-A-31PHE.sc.pdb
+Y38C: PDB-fold_wt_trem2_model0-A-38CYS.sc.pdb
 R47H: PDB-fold_wt_trem2_model0-A-47HIS.sc.pdb
