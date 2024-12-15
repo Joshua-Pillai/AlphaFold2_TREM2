0739f88641a3868810bd135cbb73d2729b5699c7
Joshua Pillai <jppillai@ucsd.edu>
2024-12-15 11:55:16 -0800

    Delete Missense3D directory

--- Missense3D/notes.py
+++
@@ -1,7 +0,0 @@
-# File Paths of Missense3D on Model 0 of AF3
-
-V27M: PDB-fold_wt_trem2_model0-A-27MET.sc.pdb
-A28V: PDB-fold_wt_trem2_model0-A-28VAL.sc.pdb
-S31F: PDB-fold_wt_trem2_model0-A-31PHE.sc.pdb
-Y38C: PDB-fold_wt_trem2_model0-A-38CYS.sc.pdb
-R47H: PDB-fold_wt_trem2_model0-A-47HIS.sc.pdb
