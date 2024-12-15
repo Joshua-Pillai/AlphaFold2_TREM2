a06a6f9c4fdf8b1aba1e463fd4a21b6d83b43856
Joshua Pillai <jppillai@ucsd.edu>
2024-12-15 12:05:05 -0800

    Create notes.py

---
+++ PDB Files/notes.py
@@ -0,0 +1,5 @@
+# The following files match the following structures.
+
+# AF-predicted WT TREM2 structure is 'AF-Q9NZC2-F1-model_v4 (3).pdb'
+#R47H Variant of AF-predicted WT TREM2 is 'PDB-af-q9nzc2-f1-model_v4_3-A-47HIS.sc.pdb'
+#RMSD data for 5ud8 and 5ud7 original structures.
