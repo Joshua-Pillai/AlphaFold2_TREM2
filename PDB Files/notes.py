c292ac6c1cb311e736cb8933ebf01ba45d34312d
Joshua Pillai <jppillai@ucsd.edu>
2024-12-15 11:56:01 -0800

    Delete PDB Files/notes.py

--- PDB Files/notes.py
+++
@@ -1,5 +0,0 @@
-# The source files for the WT TREM2 structure prediction are provided from the AlphaFold Server. 
-
-# We then took the .cif files for the 5 predictions and adjusted them to .pdb files with ChimeraX.
-
-# All subsequent analyses were conducted with PyMOL or other softwares, comparing chain A of 5UD7 to the predicted structure.
